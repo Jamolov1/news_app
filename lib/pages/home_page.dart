54850f54ef9626a71ceaafdb074319e03ac08380
jamolov1 <oktamjamolov237@gmail.com>
2025-01-27 16:04:38 +0500

    first commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:news_app/models/news_model.dart';
 import 'package:http/http.dart' as http;
@@ -12,18 +11,18 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<NewsModel> photos = [];
+  List<Article> photos = [];
 
-  Future<List<NewsModel>> getPhotoList() async {
+  Future<List<Article>> getNewsList() async {
     var response = await http.get(
       Uri.parse(
           "https://newsapi.org/v2/everything?q=tesla&from=2024-12-26&sortBy=publishedAt&apiKey=2eebeaa50122431cb3c2a603d3920bae"),
     );
     var data = jsonDecode(response.body.toString());
     List<dynamic> articlesData = data["articles"];
-    if(response.statusCode == 200){
-      for(var photo in data){
-        photos.add(NewsModel.fromJson(photo));
+    if (response.statusCode == 200) {
+      for (var article in articlesData) {
+        photos.add(Article.fromJson(article));
       }
       return photos;
     }
@@ -202,9 +201,84 @@ class _HomePageState extends State<HomePage> {
                     ],
                   ),
                 ),
-                _item_post(),
-                _item_post(),
-                _item_post(),
+                // ListView builder to display news articles
+                Container(
+                  height: MediaQuery.of(context).size.height *
+                      0.7, // define height for ListView
+                  child: FutureBuilder(
+                    future: getNewsList(),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<List<Article>> snapshot) {
+                      if (!snapshot.hasData) {
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      return ListView.builder(
+                        itemBuilder: (context, index) {
+                          var article = snapshot.data![index];
+                          return Container(
+                            margin: EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 15),
+                            height: MediaQuery.of(context).size.height * 0.13,
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  flex: 1,
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      image: DecorationImage(
+                                        image: NetworkImage(
+                                            article.urlToImage.toString()),
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 2,
+                                  child: Container(
+                                    margin: EdgeInsets.only(left: 5),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          article.author.toString(),
+                                          style: TextStyle(
+                                            color: Colors.grey.shade700,
+                                            fontSize: 16,
+                                          ),
+                                          maxLines: 1,
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                        Text(
+                                          article.description.toString(),
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w600,
+                                          ),
+                                          maxLines: 2,
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                        Text(article.publishedAt.toString())
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                        itemCount: snapshot.data?.length ?? 0,
+                      );
+                    },
+                  ),
+                ),
               ],
             ),
           ),
@@ -214,25 +288,26 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
-          fixedColor: Colors.blue,
-          unselectedItemColor: Colors.black,
-          showUnselectedLabels: true,
-          currentIndex: currenIndex,
-          onTap: (int index) {
-            setState(() {
-              currenIndex = index;
-              _controller.jumpToPage(index);
-            });
-          },
-          items: [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.explore_outlined), label: "Explore"),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.bookmark_border), label: "Bookmark"),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.person_outline), label: "Profile"),
-          ]),
+        fixedColor: Colors.blue,
+        unselectedItemColor: Colors.black,
+        showUnselectedLabels: true,
+        currentIndex: currenIndex,
+        onTap: (int index) {
+          setState(() {
+            currenIndex = index;
+            _controller.jumpToPage(index);
+          });
+        },
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.explore_outlined), label: "Explore"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.bookmark_border), label: "Bookmark"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline), label: "Profile"),
+        ],
+      ),
     );
   }
 
@@ -250,73 +325,6 @@ class _HomePageState extends State<HomePage> {
           name,
           style: TextStyle(color: Colors.grey.shade800, fontSize: 18),
         ),
-      ),
-    );
-  }
-
-  Widget _item_post() {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-      height: MediaQuery.of(context).size.height * 0.13,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Row(
-        children: [
-          Expanded(
-              child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              image: DecorationImage(
-                image: AssetImage("assets/img_2.png"),
-                fit: BoxFit.cover,
-              ),
-            ),
-          )),
-          Expanded(
-              flex: 2,
-              child: Container(
-                margin: EdgeInsets.only(left: 5),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Europe",
-                      style: TextStyle(color: Colors.grey.shade800),
-                    ),
-                    Text(
-                      "Ukraine's President Zelenskiy to BBC: Blood money being paid",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600,
-                      ),
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    Row(
-                      children: [
-                        Text(
-                          "BBC News",
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        SizedBox(
-                          width: 20,
-                        ),
-                        Text(
-                          "4h ago",
-                          style: TextStyle(color: Colors.grey.shade800),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              )),
-        ],
       ),
     );
   }
